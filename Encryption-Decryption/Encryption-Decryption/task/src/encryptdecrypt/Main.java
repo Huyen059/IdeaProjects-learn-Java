7b4fe19d791d85a30145223d80fc42ba564dd0a7
Huyen059 <samihuyen059@gmail.com>
2020-10-01 15:24:58 +0200

    finish state 2

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,46 +2,37 @@ package encryptdecrypt;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "we found a treasure!";
-        var encryption = new Encryption();
-        encryption.generateKeyPair();
-        System.out.println(encryption.encrypt(input));
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        int shiftValue = scanner.nextInt();
+        Encryption encryption = new Encryption();
+        System.out.println(encryption.encryption(input, shiftValue));
     }
 }
 
 class Encryption {
-    private Map<Character, Character> keyPair = new HashMap<>();
-
-    void generateKeyPair() {
-        generateKeyPair(1);
-    }
+    private final Map<Character, Character> keyPair = new HashMap<>();
 
     void generateKeyPair(int shiftValue) {
-        char key = 'a';
-        char value = 'z';
         for (int i = 0; i < 26; i++) {
-            this.keyPair.put(key, value);
-            for (int j = 0; j < shiftValue; j++) {
-                key++;
-                value--;
-            }
+            String alfabet = "abcdefghijklmnopqrstuvwxyz";
+            this.keyPair.put(alfabet.charAt(i), alfabet.charAt((i + shiftValue) % 26));
         }
     }
 
-    String encrypt(String input) {
+    String encryption(String input, int shiftValue) {
+        this.generateKeyPair(shiftValue);
         StringBuilder output = new StringBuilder(input);
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isLetter(input.charAt(i))) {
                 continue;
             }
-
             output.setCharAt(i, this.keyPair.get(input.charAt(i)));
         }
-
         return output.toString();
     }
 }
