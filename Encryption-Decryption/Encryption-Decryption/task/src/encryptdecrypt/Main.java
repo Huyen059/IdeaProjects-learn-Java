303788aae34fea7600faff5c9e4b411f03cf9063
Huyen059 <samihuyen059@gmail.com>
2020-10-01 15:14:58 +0200

    complet state 1/6 of Encryption Decription project

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,47 @@
 package encryptdecrypt;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String input = "we found a treasure!";
+        var encryption = new Encryption();
+        encryption.generateKeyPair();
+        System.out.println(encryption.encrypt(input));
+
+    }
+}
+
+class Encryption {
+    private Map<Character, Character> keyPair = new HashMap<>();
+
+    void generateKeyPair() {
+        generateKeyPair(1);
+    }
+
+    void generateKeyPair(int shiftValue) {
+        char key = 'a';
+        char value = 'z';
+        for (int i = 0; i < 26; i++) {
+            this.keyPair.put(key, value);
+            for (int j = 0; j < shiftValue; j++) {
+                key++;
+                value--;
+            }
+        }
+    }
+
+    String encrypt(String input) {
+        StringBuilder output = new StringBuilder(input);
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isLetter(input.charAt(i))) {
+                continue;
+            }
+
+            output.setCharAt(i, this.keyPair.get(input.charAt(i)));
+        }
+
+        return output.toString();
     }
 }
